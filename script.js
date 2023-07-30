31f58a58e81e48cc513fa35af49901f5dcb65906
Asapu Veera Venkata Vinod Kumar <82317137+vinodkumar1914@users.noreply.github.com>
2023-07-30 11:45:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl=document.getElementById("hours")
 const minutesEL=document.getElementById("minutes")
 const secondsEl=document.getElementById("seconds")
 
-const newYear ="16 Jan 2023"
+const newYear ="16 Jan 2024"
 function countdown(){
     const newYearDate=new Date(newYear);
     const currentDate= new Date();
